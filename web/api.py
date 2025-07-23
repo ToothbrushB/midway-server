fa16c0559d8e1a0c2f05d6cb104b701e321d8560
ToothbrushB <49379586+ToothbrushB@users.noreply.github.com>
2025-07-23 13:57:46 -0500

    Blueprints

--- web/api.py
+++ web/api.py
@@ -1,5 +1,13 @@
 from flask import Blueprint, request
 from mqtt import client
+import requests
+from dotenv import load_dotenv
+import os
+import json
+
+# Load environment variables from .env file
+load_dotenv()
+
 api_bp = Blueprint('api_bp', __name__)
 
 @api_bp.post('/mqtt')
@@ -10,3 +18,21 @@ def mqtt():
         return "Topic and payload are required", 400
     client.publish(topic, payload)
     return "Message published", 200
+
+@api_bp.post('/notification')
+def notification():
+    title = request.form['title']
+    content = request.form['content']
+    if not title or not content:
+        return "Title and content are required", 400
+    r = requests.post('https://api.onesignal.com/notifications', 
+                     headers={'Authorization': f"key {os.getenv('ONESIGNAL_API_KEY')}"}, json={
+                         "app_id": os.getenv('ONESIGNAL_APP_ID'),
+                         "headings": {"en": title},
+                         "contents": {"en": content},
+                        "included_segments": ["Test Users"]
+                     })
+    if r.status_code == 200 or r.status_code == 201:
+        return "Notification sent", 200
+    print(r.text)
+    return "Failed to send notification", 500
